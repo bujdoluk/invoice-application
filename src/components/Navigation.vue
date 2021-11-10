7f51e5296cb7ca609235d14ed3dbebc1f151b988
Lukáš Bujdoš <lukas.bujdos@gmail.com>
2021-11-10 17:54:45 +0100

    5th update

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,7 +1,20 @@
 <template>
   <header class="flex">
-    <div class="branding flex">
+    <div class="top flex">
       <img src="@/assets/logo.svg" alt="logo" />
+    </div>
+    <div class="bottom flex flex-column">
+      <div class="theme flex">
+        <img
+          @click="switchTheme"
+          class="theme-img"
+          src="@/assets/icon-sun.svg"
+          alt="sun"
+        />
+      </div>
+      <div>
+        <img class="avatar" src="@/assets/image-avatar.jpg" alt="avatar" />
+      </div>
     </div>
   </header>
 </template>
@@ -16,6 +29,7 @@ export default {
 header {
   z-index: 99;
   flex-direction: row;
+  justify-content: space-between;
   background-color: #1e2139;
   @media (min-width: 900px) {
     min-height: 100%;
@@ -25,7 +39,7 @@ header {
   }
 }
 
-.branding {
+.top {
   border-radius: 0 20px 20px 0;
   background-color: #7c5dfa;
   justify-content: center;
@@ -38,5 +52,30 @@ header {
     width: auto;
     height: 30px;
   }
+}
+
+.bottom {
+  justify-content: center;
+  align-items: center;
+}
+
+.theme {
+  border-bottom: 1px solid grey;
+  width: 90px;
+  justify-content: center;
+  padding: 20px;
+}
+
+.theme-img {
+  width: 35px;
+  height: 35px;
+  border-radius: 50%;
+}
+
+.avatar {
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  margin: 20px 0;
 }
 </style>
--- src/views/InvoiceView.vue
+++ src/views/InvoiceView.vue
@@ -35,7 +35,7 @@
         </button>
         <button
           v-if="currentInvoice.invoiceDraft || currentInvoice.invoicePaid"
-          @click="updateStatusToPending"
+          @click="updateStatusToPending(currentInvoice.docId)"
           class="orange"
         >
           Mark as Pending
@@ -127,7 +127,11 @@ export default {
       "TOGGLE_INVOICE",
     ]),
 
-    ...mapActions(["DELETE_INVOICE"]),
+    ...mapActions([
+      "DELETE_INVOICE",
+      "UPDATE_STATUS_TO_PENDING",
+      "UPDATE_STATUS_TO_PAID",
+    ]),
 
     getCurrentInvoice() {
       this.SET_CURRENT_INVOICE(this.$route.params.invoiceId);
@@ -142,6 +146,13 @@ export default {
     async deleteInvoice(docId) {
       await this.DELETE_INVOICE(docId);
       this.$router.push({ name: "Home" });
+    },
+
+    updateStatusToPaid(docId) {
+      this.UPDATE_STATUS_TO_PAID(docId);
+    },
+    updateStatusToPending(docId) {
+      this.UPDATE_STATUS_TO_PENDING(docId);
     },
   },
   computed: {
